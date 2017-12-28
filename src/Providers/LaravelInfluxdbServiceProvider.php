c6adc6b066c81820b1724fd5029b709b12c3a996
Simon Kleeschulte <simlux78@gmail.com>
2017-12-28 11:43:15 +0100

    added dependencies to composer.json; added Service Provider

---
+++ src/Providers/LaravelInfluxdbServiceProvider.php
@@ -0,0 +1,11 @@
+<?php declare(strict_types=1);
+
+namespace Simlux\LaravelInfluxdb;
+
+use Illuminate\Support\ServiceProvider;
+use Simlux\LaravelGenerators\Console\Commands\Generator\ModelGeneratorCommand;
+
+class LaravelGeneratorsServiceProvider extends ServiceProvider
+{
+
+}
