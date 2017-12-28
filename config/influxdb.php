3cc2dc01ce45755fca8734034c1f5067673a6ac2
Simon Kleeschulte <simlux78@gmail.com>
2017-12-28 12:04:53 +0100

    added config

--- config/influxdb.php
+++ config/influxdb.php
@@ -1,4 +1,9 @@
 <?php
 return [
-
+    'host'      => env('INFLUXDB_HOST', 'localhost'),
+    'port'      => env('INFLUXDB_PORT', '8086'),
+    'user'      => env('INFLUXDB_USER', 'user'),
+    'password'  => env('INFLUXDB_PASSWORD', 'pass'),
+    'protocol'  => env('INFLUXDB_PROTOCOL', 'tcp'), // use precision nanoseconds when using udp
+    'precision' => env('INFLUXDB_PRECISION', 's'), // SECONDS => s, NANOSECONDS = n
 ];
--- src/Providers/LaravelInfluxdbServiceProvider.php
+++ src/Providers/LaravelInfluxdbServiceProvider.php
@@ -3,9 +3,38 @@
 namespace Simlux\LaravelInfluxdb;
 
 use Illuminate\Support\ServiceProvider;
-use Simlux\LaravelGenerators\Console\Commands\Generator\ModelGeneratorCommand;
 
 class LaravelGeneratorsServiceProvider extends ServiceProvider
 {
+    private const CONFIG_FILE = __DIR__ . '/../../config/influxdb.php';
 
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->publishes([
+            self::CONFIG_FILE => config_path('influxdb.php'),
+        ]);
+        /*
+        if ($this->app->runningInConsole()) {
+            $this->commands([
+                // Class::class
+            ]);
+        }
+        */
+    }
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->mergeConfigFrom(
+            self::CONFIG_FILE, 'influxdb'
+        );
+    }
 }
